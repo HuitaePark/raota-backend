3bd52b0314e1f495acea6f174a8d9e65b202fe30
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-04 23:55:49 +0900

    Initial commit

---
+++ src/main/java/com/raota/RaotaApplication.java
@@ -0,0 +1,13 @@
+package com.raota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaotaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raota/RaotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
