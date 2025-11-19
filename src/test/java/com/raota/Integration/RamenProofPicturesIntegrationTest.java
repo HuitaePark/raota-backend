dea642e21711303290c6e7801f2d30d0ef97f021
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-19 16:59:44 +0900

    test(RamenProofPicturesIntegrationTest): 가게의 사진 리스트를 반환하는 통합테스트 구현

--- src/test/java/com/raota/Integration/RamenProofPicturesIntegrationTest.java
+++ src/test/java/com/raota/Integration/RamenProofPicturesIntegrationTest.java
@@ -2,16 +2,19 @@ package com.raota.Integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.raota.domain.member.model.MemberProfile;
 import com.raota.domain.member.repository.MemberRepository;
+import com.raota.domain.proofPicture.model.RamenProofPicture;
 import com.raota.domain.proofPicture.repository.RamenProofPictureRepository;
 import com.raota.domain.ramenShop.model.RamenShop;
 import com.raota.domain.ramenShop.repository.RamenShopRepository;
 import com.raota.global.file.FileUploader;
 import io.restassured.RestAssured;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -90,5 +93,35 @@ public class RamenProofPicturesIntegrationTest {
                 .body("data.uploader_nickname", equalTo("방금먹음"));
 
         assertThat(proofPictureRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("통합: 인증샷 목록을 페이지로 조회하면 DB의 콘텐츠를 반환한다")
+    void get_proof_pictures_returns_page_integration() {
+        RamenShop shop = ramenShopRepository.findById(targetShopId).get();
+        MemberProfile me = memberRepository.findById(memberId).get();
+
+        proofPictureRepository.save(RamenProofPicture.builder()
+                .ramenShop(shop).memberProfile(me).imageUrl("http://photo_a.com").uploadAt(LocalDateTime.now()).build());
+        proofPictureRepository.save(RamenProofPicture.builder()
+                .ramenShop(shop).memberProfile(me).imageUrl("http://photo_b.com").uploadAt(LocalDateTime.now()).build());
+
+        RestAssured.given().log().all()
+                .param("page", "0")
+                .param("size", "1")
+
+                .when()
+                .get("/photos/{shopId}", targetShopId)
+
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("status", equalTo("SUCCESS"))
+                .body("data.content", hasSize(1))
+                .body("data.content[0].uploader_nickname", equalTo("방금먹음"))
+
+                .body("data.number", equalTo(0))
+                .body("data.size", equalTo(1))
+                .body("data.totalElements", equalTo(2))
+                .body("data.totalPages", equalTo(2));
     }
 }
