7a4a2686c055da8f2f1409bc59ceae11ba264f69
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-23 03:25:12 +0900

    fix: 빈을 못찾던 문제 해결

--- src/main/java/com/raota/global/file/ImageBucketFileUploader.java
+++ src/main/java/com/raota/global/file/ImageBucketFileUploader.java
@@ -1,8 +1,10 @@
 package com.raota.global.file;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
 @Profile("prod")
 public class ImageBucketFileUploader implements FileUploader{
     @Override
