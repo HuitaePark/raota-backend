3139f086b28a77598b42491467437ce111130b2f
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-22 16:23:34 +0900

    test(MemberInfoE2ETest): 멤버 정보 E2E 테스트 생성

---
+++ src/test/java/com/raota/E2E/BaseE2ETest.java
@@ -0,0 +1,16 @@
+package com.raota.E2E;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class BaseE2ETest {
+
+    @BeforeAll
+    static void setupRestAssured() {
+        String baseUri = System.getProperty("e2e.base-uri", "http://localhost");
+        String portProp = System.getProperty("e2e.port", "8080");
+
+        RestAssured.baseURI = baseUri;
+        RestAssured.port = Integer.parseInt(portProp);
+    }
+}
